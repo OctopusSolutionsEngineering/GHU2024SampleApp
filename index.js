93b7fad1b6970df5cc8cf5f77965021c1d4ed7a7
Matthew Casperson <matthewcasperson@gmail.com>
2024-09-23 15:14:27 +1000

    Sample app now works

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
 // Define a route that renders an HTML template
 app.get('/', (req, res) => {
-    res.render('index', { message: 'Hello World!' });
+    res.render('index', { message: process.env.TITLE || 'Hello World!' });
 });
 
 app.listen(port, () => {
